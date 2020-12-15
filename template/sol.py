76969d14577c9ce2dc674d54110b42692fb7fcbe
sam <sam@sams-first-pc.fios-router.home>
2020-12-15 01:13:19 -0500

    Day 15, adding parse to template

--- template/sol.py
+++ template/sol.py
@@ -4,7 +4,14 @@ import os
 sys.path.append(f"{os.path.dirname(__file__)}/..")
 import util
 import math
+import parse
+from parse import compile
 
 fn = f"{os.path.dirname(__file__)}/in.txt"
 
+l = util.filetolist(fn)
 
+p = compile("")
+for line in l:
+    r = p.parse(line)
+    print(r.fixed)
